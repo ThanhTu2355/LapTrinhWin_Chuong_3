1e1e9e26a295ef16e009962e39ef446d0158b0ef
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-04-10 11:12:07 +0700

    code hoan thien

--- BaiTapChuong_3/BT02_GhiNFile/Form1.cs
+++ BaiTapChuong_3/BT02_GhiNFile/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
-namespace BT01_SinhVien
+namespace BT02_GhiNFile
 {
     public partial class Form1 : Form
     {
@@ -35,17 +35,19 @@ namespace BT01_SinhVien
 
         private void DocFile()
         {
-            string duong_dan = @"..\..\DuLieu\SinhVien.txt";
-            string[] mang_dong = File.ReadAllLines(duong_dan);
-            foreach (string chuoi_sv in mang_dong)
+            string duong_dan = @"..\..\DuLieu\";
+            string[] mang_duong_dan_tap_tin = Directory.GetFiles(duong_dan);
+            SinhViens.Clear();
+            foreach(string duong_dan_tap_tin in mang_duong_dan_tap_tin)
             {
+                string chuoi_sv = File.ReadAllText(duong_dan_tap_tin);
                 string[] mang_thanh_phan = chuoi_sv.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
                 SinhVien sv = new SinhVien();
                 sv.MaSV = int.Parse(mang_thanh_phan[0]);
                 sv.HoTen = mang_thanh_phan[1];
                 sv.HocBong = int.Parse(mang_thanh_phan[2]);
                 SinhViens.Add(sv);
-            }
+                }    
         }
 
         private void lstSinhVien_SelectedIndexChanged(object sender, EventArgs e)
@@ -82,33 +84,24 @@ namespace BT01_SinhVien
                 SinhVien svXoa = TimSinhVien(int.Parse(txtMaSinhVien.Text));
                 SinhViens.Remove(svXoa);
                 KhoiTaoListBox();
+                Xoa_Sinh_Vien(svXoa);
+                lstSinhVien.SelectedIndex = 0;
             }
+        }
+
+        private void Xoa_Sinh_Vien(SinhVien sv)
+        {
+            string duong_dan = @"..\..\DuLieu\"+sv.MaSV+".txt";    
+            File.Delete(duong_dan);
         }
 
         private SinhVien TimSinhVien(int msv)
         {
             SinhVien kq = null;
-            foreach(SinhVien sv in SinhViens)
+            foreach (SinhVien sv in SinhViens)
                 if (sv.MaSV == msv)
                     return sv;
-                return kq; 
-        }
-
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            Ghi_sv();
-        }
-
-        private void Ghi_sv()
-        {
-            string duong_dan = @"..\..\DuLieu\SinhVien.txt";
-            List<string> danh_sach_chuoi_sv = new List<string>();
-            foreach(SinhVien sv in SinhViens)
-            {
-                string chuoi_sv = sv.MaSV + ":" + sv.HoTen + ":" + sv.HocBong;
-                danh_sach_chuoi_sv.Add(chuoi_sv);
-            }
-            File.WriteAllLines(duong_dan, danh_sach_chuoi_sv);
+            return kq;
         }
 
         private void btnLuu_Click(object sender, EventArgs e)
@@ -123,7 +116,22 @@ namespace BT01_SinhVien
                 KhoiTaoListBox();
                 lstSinhVien.SelectedIndex = lstSinhVien.Items.IndexOf(sv);
                 txtMaSinhVien.ReadOnly = true;
+                Ghi_Sinh_Vien(sv);
+            }  else
+            {
+                SinhVien sv = TimSinhVien(int.Parse(txtMaSinhVien.Text));
+                sv.HoTen = txtHoTen.Text;
+                sv.HocBong = int.Parse(txtHocBong.Text.Replace(",", "").Replace("$", ""));
+                KhoiTaoListBox();
+                Ghi_Sinh_Vien(sv);
             }    
+        }
+
+        private void Ghi_Sinh_Vien(SinhVien sv)
+        {
+            string duong_dan = @"..\..\DuLieu\" + sv.MaSV + ".txt";
+            string chuoi_sv = sv.MaSV + ":" + sv.HoTen + ":" + sv.HocBong;
+            File.WriteAllText(duong_dan, chuoi_sv);
         }
     }
 }
---
+++ BaiTapChuong_3/BT02_GhiNFile/SinhVien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT02_GhiNFile
+{
+    class SinhVien
+    {
+        public int MaSV { get; set; }
+        public int HocBong { get; set; }
+        public string HoTen { get; set; }
+    }
+}
